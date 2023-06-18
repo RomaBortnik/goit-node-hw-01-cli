d73244429cb29e48feb79ded917447190695e409
RomaBortnik <bortnik25051998@gmail.com>
2023-06-18 15:37:56 +0300

    done

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# goit-node-hw-01-cli
+# Action = 'list'
+
+![action='list'](https://ibb.co/BKndjqV)
+
+# Action = 'get, add, remove'
+
+![action='get, add, remove'](https://ibb.co/9WHK6DD)
---
+++ index.js
@@ -0,0 +1,43 @@
+const { program } = require("commander");
+
+const contacts = require("./contacts");
+
+program
+  .option("-a, --action, <type>")
+  .option("-i, --id, <type>")
+  .option("-n, --name, <type>")
+  .option("-e, --email, <type>")
+  .option("-p, --phone, <type>");
+
+program.parse();
+
+const options = program.opts();
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const contactList = await contacts.listContacts();
+      console.log(contactList);
+      break;
+
+    case "get":
+      const contact = await contacts.getContactById(id);
+      console.log(contact);
+      break;
+
+    case "add":
+      const newContact = await contacts.addContact(name, email, phone);
+      console.log(newContact);
+      break;
+
+    case "remove":
+      const removedContact = await contacts.removeContact(id);
+      console.log(removedContact);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(options);
